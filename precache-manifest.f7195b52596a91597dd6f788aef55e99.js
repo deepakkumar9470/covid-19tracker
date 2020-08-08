65f73f12e8c261c043d8af00366341e3e87ffb2e
Deepak Kumar Chouhan <dk397787@gmail.com>
2020-08-08 11:30:12 +0530

    Updates

---
+++ precache-manifest.f7195b52596a91597dd6f788aef55e99.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abdad449da25a7fcf305be050f1066d1",
+    "url": "/covid-19tracker/index.html"
+  },
+  {
+    "revision": "7a8f2081f99fcb7ce9bb",
+    "url": "/covid-19tracker/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9e31f0579edde2a39fcf",
+    "url": "/covid-19tracker/static/css/main.d83e6544.chunk.css"
+  },
+  {
+    "revision": "7a8f2081f99fcb7ce9bb",
+    "url": "/covid-19tracker/static/js/2.de4fad7f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19tracker/static/js/2.de4fad7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e31f0579edde2a39fcf",
+    "url": "/covid-19tracker/static/js/main.614ad1b9.chunk.js"
+  },
+  {
+    "revision": "0803e0ac29db9c24e369",
+    "url": "/covid-19tracker/static/js/runtime-main.ae81f933.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19tracker/static/media/covid19.d7265326.png"
+  }
+]);
